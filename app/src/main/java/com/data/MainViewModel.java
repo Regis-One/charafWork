dc97f0e595ec0bfa430cbc8103ad00daa45fae30
Charaf Idrissi <idri0022@algonquinlive.com>
2023-09-25 19:24:02 -0400

    changes made

---
+++ app/src/main/java/com/data/MainViewModel.java
@@ -0,0 +1,11 @@
+package com.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    public MutableLiveData<String> editString = new MutableLiveData<>();
+
+
+}
